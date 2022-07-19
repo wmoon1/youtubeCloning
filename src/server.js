3a261d5c16d9b7c2e2326f9bd9d6454f75c3abd1
Wooram Moon <wmoon0022@gmail.com>
2022-07-19 09:28:51 -0400

    add webpack

--- src/server.js
+++ src/server.js
@@ -33,7 +33,9 @@ app.use(
 
 
 app.use(localsMiddleware);
-app.use("/uploads", express.static("uploads"))
+app.use("/uploads", express.static("uploads"));
+// first "" is URL, and second "" is folder
+app.use("/static", express.static("assets"));
 app.use("/", rootRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
